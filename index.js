79dbc063d9520239504f1e1a6d53da5dfbb288f0
elliott-steven <69441444+elliott-steven@users.noreply.github.com>
2021-03-05 17:19:38 -0500

    creating questions for each role

--- index.js
+++ index.js
@@ -1,6 +1,61 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+const Employee = require("./lib/employee")
 const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
+
+let employee = [];
+let manager = [];
+let engineer = [];
+let intern = [];
+
+function Prompt() {
+
+    return inquirer
+
+        .prompt([
+            {
+                type: 'text',
+                name: 'employee',
+                message: "What is the employee's name?"
+            },
+            {
+                type: 'text',
+                name: 'id',
+                message: "What is the employee's ID number?"
+            },
+            {
+                type: 'text',
+                name: 'email',
+                message: "What is the employee's email address?"
+            },
+            {
+                type: 'list',
+                name: 'role',
+                message: "What is the employee's role on this team?"
+            choices: ['Manager', 'Engineer', 'Intern']
+            },
+        ])
+
+        .then(({ employee, id, email, role }) => {
+
+            if (role === "Manager") {
+                return inquirer
+                    .prompt([{
+                        type: 'text',
+                        name: 'officeNum',
+                        message: "What is your office phone number?"
+                    },
+                    {
+                        type: 'confirm',
+                        name: 'anotherEmp',
+                        message: "Would you like to add another employee?",
+                        default: false
+                    }
+                    ])
+            }
+
+        })
+}
